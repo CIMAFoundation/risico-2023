22d60dd1151f46c7463a90ae040bb2041461e7ec
mirkodandrea <mirko.dandrea@gmail.com>
2024-03-06 11:16:03 +0100

    Add Docker build and run commands

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Building and running the application with Docker"
+echo "Building the Docker image"
 docker build -t risico-2023 .
+echo "Running the Docker container"
 docker run --rm -v $(pwd):/app risico-2023
 
