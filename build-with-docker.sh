b7caee0c55ee1ce791e871631daa275cfb25a10c
mirkodandrea <mirko.dandrea@gmail.com>
2024-03-09 09:32:23 +0100

    build: Refactor build-on-release.yml and build-with-docker.sh scripts

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -4,5 +4,6 @@ echo "Building the Docker image"
 docker build -t risico-2023 .
 echo "Running the Docker container"
 docker run --rm -v $(pwd):/app risico-2023
+chmod +x target/x86_64-unknown-linux-gnu/release/risico-2023
 echo "Done"
 
