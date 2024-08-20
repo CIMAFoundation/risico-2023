ec7154680d41537f25f93f2d7374a2254758f947
mirkodandrea <mirko.dandrea@gmail.com>
2024-08-20 13:57:05 +0200

    Bump version to v0.2.0

--- src/library/version.rs
+++ src/library/version.rs
@@ -1,7 +1,7 @@
-pub const FULL_VERSION: &str = "v0.2.0-eef76a1";
+pub const FULL_VERSION: &str = "v0.2.0-6d8c982";
 pub const LONG_VERSION: &str = "
 version: v0.2.0
-commit: eef76a1214268be557dad55d92ad242446bdbdf8
+commit: 6d8c98225545368a6287c43bddd023575d0ad737
 branch: main
-released on: Tue, 20 Aug 2024 13:38:02 +0200";
+released on: Tue, 20 Aug 2024 13:57:05 +0200";
 
