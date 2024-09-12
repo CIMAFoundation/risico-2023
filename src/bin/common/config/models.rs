3df9ba483f41fe152c4acd8b7d46e79fb482ac6c
Perello-nico <perellonicolo@gmail.com>
2024-09-12 10:45:04 +0200

    moved functions from data to specific model configurations in bin/common/config/model.rs

--- src/bin/common/config/models.rs
+++ src/bin/common/config/models.rs
@@ -1,8 +1,12 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{self, BufRead, BufWriter, Write},
+    io::{self, BufRead, BufReader, BufWriter, Write},
 };
+
+use std::f32::consts::PI;
+use std::fs;
+use std::sync::Arc;
 
 use chrono::*;
 use chrono::{DateTime, Utc};
@@ -13,18 +17,15 @@ use risico::{
     models::output::Output,
     modules::risico::{
         config::RISICOModelConfig,
-        models::{RISICOProperties, RISICOState, RISICOWarmState},
+        models::{RISICOCellPropertiesContainer, RISICOProperties, RISICOVegetation, RISICOState, RISICOWarmState},
     },
     modules::fwi::{
         config::FWIModelConfig,
-        models::{FWIProperties, FWIState, FWIWarmState},
+        models::{FWICellPropertiesContainer, FWIProperties, FWIState, FWIWarmState},
     }
 };
 
-use super::{
-    builder::{OutputTypeConfig, RISICOConfigBuilder, FWIConfigBuilder},
-    data::{risico_properties_from_file, read_vegetation, fwi_properties_from_file},
-};
+use super::builder::{OutputTypeConfig, RISICOConfigBuilder, FWIConfigBuilder};
 use crate::common::helpers::RISICOError;
 use crate::common::io::models::{output::OutputType, palette::Palette};
 
@@ -102,7 +103,7 @@ impl RISICOConfig {
 
         let cells_file = &config_defs.cells_file_path;
 
-        let props_container = risico_properties_from_file(cells_file)
+        let props_container = RISICOConfig::properties_from_file(cells_file)
             .map_err(|error| format!("error reading {}, {error}", cells_file))?;
 
         let n_cells = props_container.lons.len();
@@ -114,7 +115,7 @@ impl RISICOConfig {
             panic!("All properties must have the same length");
         }
 
-        let vegetations_dict = read_vegetation(&config_defs.vegetation_file)
+        let vegetations_dict = RISICOConfig::read_vegetation(&config_defs.vegetation_file)
             .map_err(|error| format!("error reading {}, {error}", &config_defs.vegetation_file))?;
 
         let (warm_state, warm_state_time) = RISICOConfig::read_warm_state(&config_defs.warm_state_path, date)
@@ -150,6 +151,152 @@ impl RISICOConfig {
         };
 
         Ok(config)
+    }
+
+    /// Read the cells from a file.
+    /// :param file_path: The path to the file.
+    /// :return: A list of cells.
+    pub fn properties_from_file(file_path: &str) -> Result<RISICOCellPropertiesContainer, RISICOError> {
+        let file = fs::File::open(file_path).map_err(|err| format!("can't open file: {err}."))?;
+    
+        let mut lons: Vec<f32> = Vec::new();
+        let mut lats: Vec<f32> = Vec::new();
+        let mut slopes: Vec<f32> = Vec::new();
+        let mut aspects: Vec<f32> = Vec::new();
+        let mut vegetations: Vec<String> = Vec::new();
+    
+        let reader = BufReader::new(file);
+    
+        for line in reader.lines() {
+            let line = line.map_err(|err| format!("can't read from file: {err}."))?;
+            if line.starts_with("#") {
+                // skip header
+                continue;
+            }
+    
+            let line_parts: Vec<&str> = line.trim().split(' ').collect();
+    
+            if line_parts.len() < 5 {
+                let error_message = format!("Invalid line in file: {}", line);
+                return Err(error_message.into());
+            }
+    
+            //  [TODO] refactor this for using error handling
+            let lon = line_parts[0]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+    
+            let lat = line_parts[1]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+    
+            let slope = line_parts[2]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            let aspect = line_parts[3]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+    
+            let vegetation = line_parts[4].to_string();
+    
+            let slope = slope * PI / 180.0;
+            let aspect = aspect * PI / 180.0;
+    
+            lons.push(lon);
+            lats.push(lat);
+            slopes.push(slope);
+            aspects.push(aspect);
+            vegetations.push(vegetation);
+        }
+    
+        let props = RISICOCellPropertiesContainer {
+            lats,
+            lons,
+            slopes,
+            aspects,
+            vegetations,
+        };
+        Ok(props)
+    }
+
+    /// Read the cells from a file.
+    /// :param file_path: The path to the file.
+    /// :return: A list of cells.
+    pub fn read_vegetation(
+        file_path: &str,
+    ) -> Result<HashMap<String, Arc<RISICOVegetation>>, std::io::Error> {
+        let file = fs::File::open(file_path)?;
+        let mut vegetations: HashMap<String, Arc<RISICOVegetation>> = HashMap::new();
+
+        let reader = BufReader::new(file);
+
+        for (i, line) in reader.lines().enumerate() {
+            let line = line?;
+            if i == 0 && line.starts_with("#") || line.is_empty() {
+                // skip header and empty lines
+                continue;
+            }
+            let line_elements: Vec<&str> = line.split_whitespace().collect::<Vec<&str>>();
+
+            let n_elements = line_elements.len();
+            if n_elements < 9 {
+                let error_message = format!("Invalid line in file: {}", line);
+                let error = std::io::Error::new(std::io::ErrorKind::InvalidData, error_message);
+                return Err(error);
+            }
+
+            //  [TODO] refactor this for using error handling
+            let id = line_elements[0].to_string();
+            let d0 = line_elements[1]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            let d1 = line_elements[2]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            let hhv = line_elements[3]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            let umid = line_elements[4]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            let v0 = line_elements[5]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            #[allow(non_snake_case)]
+            let T0 = line_elements[6]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+            let sat = line_elements[7]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+
+            let use_ndvi = match n_elements {
+                10.. => line_elements[8]
+                    .parse::<bool>()
+                    .unwrap_or_else(|_| panic!("Invalid line in file: {}", line)),
+                _ => false,
+            };
+            let name = line_elements[n_elements - 1].to_string();
+
+            let veg_id = id.clone();
+
+            let veg = Arc::new(RISICOVegetation {
+                id,
+                d0,
+                d1,
+                hhv,
+                umid,
+                v0,
+                T0,
+                sat,
+                name,
+                use_ndvi,
+            });
+
+            vegetations.insert(veg_id, veg);
+        }
+
+        Result::Ok(vegetations)
     }
 
     pub fn get_properties(&self) -> &RISICOProperties {
@@ -327,7 +474,7 @@ impl FWIConfig {
 
         let cells_file = &config_defs.cells_file_path;
 
-        let props_container = fwi_properties_from_file(cells_file)
+        let props_container = FWIConfig::properties_from_file(cells_file)
             .map_err(|error| format!("error reading {}, {error}", cells_file))?;
 
         let n_cells = props_container.lons.len();
@@ -359,6 +506,48 @@ impl FWIConfig {
         };
 
         Ok(config)
+    }
+
+    pub fn properties_from_file(file_path: &str) -> Result<FWICellPropertiesContainer, RISICOError> {
+        let file = fs::File::open(file_path).map_err(|err| format!("can't open file: {err}."))?;
+    
+        let mut lons: Vec<f32> = Vec::new();
+        let mut lats: Vec<f32> = Vec::new();
+    
+        let reader = BufReader::new(file);
+    
+        for line in reader.lines() {
+            let line = line.map_err(|err| format!("can't read from file: {err}."))?;
+            if line.starts_with("#") {
+                // skip header
+                continue;
+            }
+    
+            let line_parts: Vec<&str> = line.trim().split(' ').collect();
+    
+            if line_parts.len() < 2 {
+                let error_message = format!("Invalid line in file: {}", line);
+                return Err(error_message.into());
+            }
+    
+            //  [TODO] refactor this for using error handling
+            let lon = line_parts[0]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+    
+            let lat = line_parts[1]
+                .parse::<f32>()
+                .unwrap_or_else(|_| panic!("Invalid line in file: {}", line));
+    
+            lons.push(lon);
+            lats.push(lat);
+        }
+    
+        let props = FWICellPropertiesContainer {
+            lats,
+            lons,
+        };
+        Ok(props)
     }
 
     pub fn get_properties(&self) -> &FWIProperties {
