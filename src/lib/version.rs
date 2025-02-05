a699409110f096746045b2053c9ce19e93c30340
mirkodandrea <mirko.dandrea@gmail.com>
2025-02-05 14:49:55 +0100

    Bump version to v0.4.0

--- src/lib/version.rs
+++ src/lib/version.rs
@@ -1,7 +1,7 @@
-pub const FULL_VERSION: &str = "v0.3.4-1da5cea";
+pub const FULL_VERSION: &str = "v0.4.0-0155569";
 pub const LONG_VERSION: &str = "
-version: v0.3.4
-commit: 1da5ceae71735db64797dcc1dc90b9a072241aee
+version: v0.4.0
+commit: 0155569bab8cd48635d0c19f895a5165f566fe80
 branch: main
-released on: Thu, 10 Oct 2024 17:45:48 +0200";
+released on: Wed, 05 Feb 2025 14:49:55 +0100";
 
