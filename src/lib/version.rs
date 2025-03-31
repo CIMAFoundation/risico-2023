ca50d6449c97df4dc4357ca2c63e231c9babf43e
mirkodandrea <mirko.dandrea@gmail.com>
2025-03-31 11:51:38 +0200

    Bump version to v1.0.0

--- src/lib/version.rs
+++ src/lib/version.rs
@@ -1,7 +1,7 @@
-pub const FULL_VERSION: &str = "v1.0.1-f843b65";
+pub const FULL_VERSION: &str = "v1.0.0-c68febe";
 pub const LONG_VERSION: &str = "
-version: v1.0.1
-commit: f843b65f6bfd60495003ded5d4a7212eacc7c913
+version: v1.0.0
+commit: c68febee9ba6880681c0041beee9e4fd10104bf7
 branch: main
-released on: Mon, 31 Mar 2025 11:50:20 +0200";
+released on: Mon, 31 Mar 2025 11:51:38 +0200";
 
