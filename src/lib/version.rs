7b3dff7ace2f56b2048a8f7e341966f7bdfd3ab5
Perello-nico <perellonicolo@gmail.com>
2026-04-17 18:34:25 +0200

    Bump version to v1.0.6

--- src/lib/version.rs
+++ src/lib/version.rs
@@ -1,7 +1,7 @@
-pub const FULL_VERSION: &str = "v1.0.6-0eb7093";
+pub const FULL_VERSION: &str = "v1.0.6-6d0cee1";
 pub const LONG_VERSION: &str = "
 version: v1.0.6
-commit: 0eb709353a33b47c74817bd2365d67d1a070d7ca
+commit: 6d0cee1cdd0992258f19ec10f0dcb8e34b423cf5
 branch: main
-released on: Tue, 07 Apr 2026 19:43:26 +0200";
+released on: Fri, 17 Apr 2026 18:34:25 +0200";
 
